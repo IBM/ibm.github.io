852d57c6fd9e54af6a63190b00c78efe7d41672e
Peter Stein <peter.stein1@ibm.com>
2019-01-06 14:36:32 +0100

    Adding new beta page ~currently just re-designed and removed most of the functions

---
+++ beta/js/orgs.js
@@ -0,0 +1,345 @@
+var orgs = [
+  {
+      "name": "csync/TogetherStream",
+      "type": "repo"
+  },
+  {
+      "name": "bradleyjeck/epanetReader",
+      "type": "repo"
+  },
+  {
+      "name": "wasdev",
+      "type": "org",
+      "link": "http://wasdev.github.io/"
+  },
+  {
+      "name": "acmeair",
+      "type": "org"
+  },
+  {
+      "name": "kimchi-project",
+      "type": "org"
+  },
+  {
+      "name": "OpenSocial",
+      "type": "org"
+  },
+  {
+      "name": "softlayer",
+      "type": "org"
+  },
+  {
+      "name": "cloudant",
+      "type": "org"
+  },
+  {
+      "name": "cloudant-labs",
+      "type": "org"
+  },
+  {
+      "name": "node-red",
+      "type": "org"
+  },
+  {
+      "name": "ibmjstart",
+      "type": "org"
+  },
+  {
+      "name": "ibm-js",
+      "type": "org"
+  },
+  {
+      "name": "IBMStreams",
+      "type": "org"
+  },
+  {
+      "name": "ibm-bioinformatics",
+      "type": "user"
+  },
+  {
+      "name": "ld4apps",
+      "type": "org"
+  },
+  {
+      "name": "wsadminlib",
+      "type": "user"
+  },
+  {
+      "name": "apimanagement",
+      "type": "org"
+  },
+  {
+      "name": "Watson-Explorer",
+      "type": "org"
+  },
+  {
+      "name": "ot4i/event-filter-pattern",
+      "type": "repo"
+  },
+  {
+      "name": "ot4i/mqtt-client-connector",
+      "type": "repo"
+  },
+  {
+      "name": "beloglazov/hamcrest-querydsl",
+      "type": "repo"
+  },
+  {
+      "name": "beloglazov/couchdb-scala",
+      "type": "repo"
+  },
+  {
+      "name": "ibmruntimes/node",
+      "type": "repo"
+  },
+  {
+      "name": "ibmruntimes/v8ppc",
+      "type": "repo"
+  },
+  {
+      "name": "ibmruntimes/v8z",
+      "type": "repo"
+  },
+  {
+      "name": "ibm-rtvs",
+      "type": "user"
+  },
+  {
+      "name": "bigfix",
+      "type": "org"
+  },
+  {
+      "name": "Open-I-Beam",
+      "type": "org"
+  },
+  {
+      "name": "fog/fog-softlayer",
+      "type": "repo"
+  },
+  {
+      "name": "y-omura/TransProg",
+      "type": "repo"
+  },
+  {
+      "name": "IBM-Cloud",
+      "type": "org"
+  },
+  {
+      "name": "watson-developer-cloud",
+      "type": "org"
+  },
+  {
+      "name": "IBM-Watson",
+      "type": "org"
+  },
+  {
+      "name": "ibm-research",
+      "type": "org"
+  },
+  {
+      "name": "nadgowdas/cargo",
+      "type": "repo"
+  },
+  {
+      "name": "digexp",
+      "type": "org"
+  },
+  {
+      "name": "andybalaam/git-history-data",
+      "type": "repo"
+  },
+  {
+      "name": "ibm-cds-labs",
+      "type": "org"
+  },
+  {
+      "name": "OpenWhisk",
+      "type": "org"
+  },
+  {
+      "name": "IBM-DBWKL",
+      "type": "org"
+  },
+  {
+      "name": "apache/systemml",
+      "type": "repo"
+  },
+  {
+      "name": "node-red/node-red",
+      "type": "repo"
+  },
+  {
+      "name": "Brunel-Visualization/Brunel",
+      "type": "repo"
+  },
+  {
+      "name": "J9Java/panama-layout-prototype",
+      "type": "repo"
+  },
+  {
+      "name": "xdevops/ibm-containers-emulator",
+      "type": "repo"
+  },
+  {
+      "name": "ibmalchemy",
+      "type": "org"
+  },
+  {
+      "name": "jasnell/activitystrea.ms",
+      "type": "repo"
+  },
+  {
+      "name": "openstack/storlets",
+      "type": "repo"
+  },
+  {
+      "name": "maximilien/i18n4go",
+      "type": "repo"
+  },
+  {
+      "name": "ibm-et",
+      "type": "org"
+  },
+  {
+      "name": "RuntimeTools/appmetrics",
+      "type": "repo"
+  },
+  {
+      "name": "gaiandb/gaiandb",
+      "type": "repo"
+  },
+  {
+      "name": "cloudviz/agentless-system-crawler",
+      "type": "repo"
+  },
+  {
+      "name": "shmul/mule",
+      "type": "repo"
+  },
+  {
+      "name": "ibmstorage",
+      "type": "org"
+  },
+  {
+      "name": "djwillia/solo5",
+      "type": "repo"
+  },
+  {
+      "name": "quarks-edge",
+      "type": "org"
+  },
+  {
+      "name": "jamiejennings/rosie-pattern-language",
+      "type": "repo"
+  },
+  {
+      "name": "openblockchain",
+      "type": "org"
+  },
+  {
+      "name": "aruniyengar/storage-client-library",
+      "type": "repo"
+  },
+  {
+      "name": "IBM-MIL",
+      "type": "org"
+  },
+  {
+      "name": "cognitive-catalyst",
+      "type": "org"
+  },
+  {
+      "name": "ibm-design",
+      "type": "org"
+  },
+  {
+      "name": "presence-insights",
+      "type": "org"
+  },
+  {
+      "name": "ibm-bluemix-mobile-services",
+      "type": "org"
+  },
+  {
+      "name": "ibm-security-innovation",
+      "type": "org"
+  },
+  {
+      "name": "cicsdev",
+      "type": "org"
+  },
+  {
+      "name": "strongloop",
+      "type": "org"
+  },
+  {
+      "name": "personality-insights",
+      "type": "org"
+  },
+  {
+      "name": "zosconnect",
+      "type": "org"
+  },
+  {
+      "name": "IBM-DSE",
+      "type": "org"
+  },
+  {
+      "name": "IBMResilient",
+      "type": "org"
+  },
+  {
+      "name": "ComputationalGenomics",
+      "type": "org"
+  },
+  {
+      "name": "ibm-watson-iot",
+      "type": "org"
+  },
+  {
+      "name": "IzODA",
+      "type": "org"
+  },
+  {
+      "name": "ibm-research-tokyo/dybm",
+      "type": "repo"
+  },
+  {
+      "name": "QISKit",
+      "type": "org",
+      "link": "https://www.qiskit.org/"
+  },
+  {
+      "name": "CODAIT",
+      "type": "org",
+      "link": "http://codait.org/"
+  },
+  {
+      "name": "OpenLiberty",
+      "type": "org",
+      "link": "https://openliberty.io/"
+  },
+  {
+      "name": "ibm-s390-tools",
+      "type": "org"
+  },
+  {
+      "name": "ibm-security-mss",
+      "type": "org"
+  },
+  {
+      "name": "IBM/API4JSON",
+      "type": "repo"
+  },
+  {
+      "name": "ruscur/snowpatch",
+      "type": "repo"
+  },
+  {
+      "name": "ibm-messaging",
+      "type": "org"
+  },
+  {   "name": "IBM",
+      "type": "org"
+  }
+];
